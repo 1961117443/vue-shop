065887533f9063bd6d66450a67d0c0c7a10d4da4
1961117443 <1961117443@qq.com>
2019-12-05 00:01:56 +0800

    新增选择资料的组件

---
+++ src/components/ChooseItem.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="choose-item-container" ref="container"> 
+        <van-sticky :container="container" :z-index="99">
+            <van-nav-bar
+            title="包装方式"
+            left-text="返回"
+            right-text="确定"
+            left-arrow
+            @click-left="onClickLeft"
+            @click-right="onClickRight"
+            />
+            <van-search placeholder="请输入搜索关键词" v-model="value" />
+        </van-sticky>
+        
+        <van-radio-group v-model="check">
+            <van-cell-group>
+                <van-cell v-for="item in list" :key="item.id" clickable @click="check=item.id" class="van-ellipsis--l2">
+                    <van-radio :name="item.id">{{item.text}}</van-radio>
+                </van-cell>
+            </van-cell-group>
+        </van-radio-group>
+    </div>
+</template>
+
+<script>
+import {  Cell,CellGroup,RadioGroup,Radio,NavBar,Sticky,Search    } from 'vant';
+    export default {
+        name:'ChooseItem',
+        components:{
+            [CellGroup.name]:CellGroup,
+            [RadioGroup.name]:RadioGroup,
+            [Radio.name]:Radio,
+            [Cell.name]:Cell,
+            [NavBar.name]:NavBar,
+            [Sticky.name]:Sticky,
+            [Search.name]:Search
+        },
+        data(){
+            return{
+                check:this.data,
+                container:null,
+                value:""
+            }
+        },
+        props:['list','data'],
+        mounted(){
+            this.container = this.$refs.container
+        },
+        methods:{
+            onClickLeft(){
+
+            },
+            onClickRight(){ 
+                this.$emit('onChoose', this.list[this.check])
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.choose-item-container{
+
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,32 +6,80 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell title="商品详情页面" is-link to="/goods/info" />
+    <van-cell-group class="cell-test-group">
+      <van-sticky>
+        <van-cell title="商品详情页面" is-link to="/goods/info" />
+      </van-sticky>
+      
+      <van-cell title="测试数据绑定"  @click="testBindClick" :value="order.bzName"/>
+
+    </van-cell-group>
       <van-cell
         v-for="item in list"
         :key="item"
         :title="item"
       />
     </van-list>
+
+    <!-- 测试绑定数据空间 -->
+    <van-popup
+      v-model="showBindPopup"
+      position="right"
+      :style="{ height: '100vh',width:'100%' }"
+    >
+      <choose-item 
+      :list="list2" 
+      :data="order.k1" 
+      @onChoose="onChoose"
+      >
+      </choose-item>
+    </van-popup>
+
   </div>
 </template>
 
 <script>
-  import { List,Cell } from 'vant';
+  import { List,Cell,CellGroup,Popup,Sticky  } from 'vant';
+  import ChooseItem from '@/components/ChooseItem.vue'
   export default {
     components:{
       [List.name]:List,
-      [Cell.name]:Cell
+      [Cell.name]:Cell,
+      [ChooseItem.name]:ChooseItem,
+      [Popup.name]:Popup,
+      [CellGroup.name]:CellGroup,
+      [Sticky.name]:Sticky
     },
     data() {
       return {
         list: [],
+        list2: [],
         loading: false,
-        finished: false
+        finished: false,
+        showBindPopup:false,
+        order:{
+          k1:-1,
+          bzName:""
+        }
       };
     },
-
+    mounted(){
+      this.getPackingCategory()
+    },
     methods: {
+      testBindClick(){
+        this.showBindPopup = true
+      },
+      getPackingCategory(){
+        this.$http.get('/api/packing/category').then(res=>{
+          this.list2 = res
+        })
+      },
+      onChoose(data){
+        this.order.bzName = data.text
+        this.order.k1 = data.id 
+        this.showBindPopup = false;
+      },
       onLoad() {
         // 异步更新数据
         setTimeout(() => {
@@ -52,5 +100,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .cell-test-group{
+    text-align: left;
+  }
 </style>
